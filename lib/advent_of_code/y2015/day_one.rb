eb2db16bef4c29156801dc63926537e607495341
Carlos Soria <csoria@cultome.io>
2020-12-03 21:53:02 -0600

    Start templating

---
+++ lib/advent_of_code/y2015/day_one.rb
@@ -0,0 +1,11 @@
+def first
+  "first part"
+end
+
+def second
+  "second part"
+end
+
+def input
+  open('inputs/y2015/day_one.txt').map { |line|  }
+end
---
+++ lib/advent_of_code/y2020/day_one.rb
@@ -0,0 +1,11 @@
+def first
+  "first part"
+end
+
+def second
+  "second part"
+end
+
+def input
+  open('inputs/y2020/day_one.txt').map { |line|  }
+end
