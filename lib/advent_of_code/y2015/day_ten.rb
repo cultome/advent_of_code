387482c0fe6f48f227d4f76b40bb4d378db1db29
Carlos Soria <csoria@cultome.io>
2021-11-15 09:54:32 -0600

    Solve 2015, day 10

---
+++ lib/advent_of_code/y2015/day_ten.rb
@@ -0,0 +1,22 @@
+def first
+  value = input.first
+  40.times { value = process value }
+  value.size
+end
+
+def second
+  value = input.first
+  50.times { value = process value }
+  value.size
+end
+
+def process(value)
+  segments = value.split('').slice_when { |a,b| a != b }.map(&:join)
+  value = segments.map do |seg|
+    "#{seg.size}#{seg[0]}"
+  end.join()
+end
+
+def input
+  open('inputs/y2015/day_ten.txt').map(&:chomp)
+end
