729f567e9f02d18ee1d9d4fbcca742b59dc7f63a
Carlos Soria <csoria@cultome.io>
2021-11-15 00:05:55 -0600

    Solve 2015, day 8

---
+++ lib/advent_of_code/y2015/day_eight.rb
@@ -0,0 +1,27 @@
+def first
+  input.sum do |line|
+    chars_count = line.split('').size
+    memory_count = line
+      .gsub(/^"/, '')
+      .gsub(/"$/, '')
+      .gsub('\\\\', '-')
+      .gsub('\\"', '-')
+      .gsub(/\\x[a-f0-9]{2}/, '-')
+      .size
+
+    chars_count - memory_count
+  end
+end
+
+def second
+  input.sum do |line|
+    chars_count = line.split('').size
+    encoded_size = line.inspect.size
+
+    encoded_size - chars_count
+  end
+end
+
+def input
+  open('inputs/y2015/day_eight.txt').map(&:chomp)
+end
