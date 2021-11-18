9c20fa66ecfd55a30a443ffb45df81497f0f8c01
Carlos Soria <csoria@cultome.io>
2021-11-18 00:21:36 -0600

    Solve 2016, day 6

---
+++ lib/advent_of_code/y2016/day_six.rb
@@ -0,0 +1,27 @@
+def first
+  input.each_with_object(Hash.new{|h,k| h[k] = []}) do |vals, acc|
+    vals.each_with_index do |letter, idx|
+      acc[idx] << letter
+    end
+  end.transform_values do |col|
+    grp = col.group_by { |letter| letter }.transform_values(&:size)
+    max = grp.values.max
+    grp.find { |_, v| v == max }.first
+  end.values.join
+end
+
+def second
+  input.each_with_object(Hash.new{|h,k| h[k] = []}) do |vals, acc|
+    vals.each_with_index do |letter, idx|
+      acc[idx] << letter
+    end
+  end.transform_values do |col|
+    grp = col.group_by { |letter| letter }.transform_values(&:size)
+    max = grp.values.min
+    grp.find { |_, v| v == max }.first
+  end.values.join
+end
+
+def input
+  open('inputs/y2016/day_six.txt').map(&:chomp).map { |line| line.split('') }
+end
