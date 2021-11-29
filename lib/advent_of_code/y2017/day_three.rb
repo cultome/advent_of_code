f47df4f4786467bec123903ae2531ce9a2892016
Carlos Soria <csoria@cultome.io>
2021-11-29 00:05:12 -0600

    Refactor a bit 2017, day 3

--- lib/advent_of_code/y2017/day_three.rb
+++ lib/advent_of_code/y2017/day_three.rb
@@ -1,45 +1,30 @@
 def first
-  input = 347991
-
-  directions = %i[right up left down]
-  dir = 0
-  x, y = 0, 0
-  steps = 1
-  current_steps = 0
-  current_segment = 1
-
-  1.upto(input) do |num|
-    case directions[dir]
-    when :right
-      x += 1
-    when :left
-      x -= 1
-    when :up
-      y -= 1
-    when :down
-      y += 1
-    end
-
-    return x.abs + y.abs if num + 1 == input
-
-    current_steps += 1
-
-    if current_steps % steps == 0
-      dir = (dir + 1) % directions.size
-      current_steps = 0
-      current_segment += 1
-    end
-
-    if current_segment % 3 == 0
-      current_segment = 1
-      steps += 1
-    end
-  end
+  spiral do |num, x, y|
+    num == 347991
+  end.map(&:abs).sum
 end
 
 def second
-  input = 347991
+  mem = { "(0,0)" => 1 }
 
+  x, y = spiral do |num, x, y|
+    mem["(#{x},#{y})"] =
+      mem.fetch("(#{x-1},#{y-1})", 0) +
+      mem.fetch("(#{x},#{y-1})", 0) +
+      mem.fetch("(#{x+1},#{y-1})", 0) +
+      mem.fetch("(#{x-1},#{y})", 0) +
+      mem.fetch("(#{x+1},#{y})", 0) +
+      mem.fetch("(#{x-1},#{y+1})", 0) +
+      mem.fetch("(#{x},#{y+1})", 0) +
+      mem.fetch("(#{x+1},#{y+1})", 0)
+
+    mem["(#{x},#{y})"] >= 347991
+  end
+
+  mem["(#{x},#{y})"]
+end
+
+def spiral
   directions = %i[right up left down]
   dir = 0
   x, y = 0, 0
@@ -63,17 +48,7 @@ def second
       y += 1
     end
 
-    mem["(#{x},#{y})"] =
-      mem.fetch("(#{x-1},#{y-1})", 0) +
-      mem.fetch("(#{x},#{y-1})", 0) +
-      mem.fetch("(#{x+1},#{y-1})", 0) +
-      mem.fetch("(#{x-1},#{y})", 0) +
-      mem.fetch("(#{x+1},#{y})", 0) +
-      mem.fetch("(#{x-1},#{y+1})", 0) +
-      mem.fetch("(#{x},#{y+1})", 0) +
-      mem.fetch("(#{x+1},#{y+1})", 0)
-
-    return mem["(#{x},#{y})"] if mem["(#{x},#{y})"] >= input
+    return [x, y] if yield(num + 1, x, y)
 
     current_steps += 1
 
